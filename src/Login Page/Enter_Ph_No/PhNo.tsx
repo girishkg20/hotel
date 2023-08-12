f547a38139202254a5e7b8b841beb8b7a8d55306
girishkg20 <girishkg20@gmail.com>
2023-08-12 21:29:13 +0530

    login page try 3

--- src/Login Page/Enter_Ph_No/PhNo.tsx
+++ src/Login Page/Enter_Ph_No/PhNo.tsx
@@ -10,6 +10,8 @@ const Phno = () => {
     const [resetbutton, setresetbutton] = useState<HTMLButtonElement | null>();
     const [enterednumber, setenterednumber] = useState<HTMLInputElement | null>();
     const [boxboarder, setboxboarder] = useState<HTMLFormElement | null>();
+    const [invalidphnoerr, setinvalidphnoerr] = useState<any>();
+    const [OTPbuttonstate, setOTPbuttonstate] =useState<boolean>(true);
 
     useEffect(() => {
         setresetbutton(document.getElementById("telrbtn") as HTMLButtonElement | null);
@@ -19,7 +21,14 @@ const Phno = () => {
 
     function valuecheck() {
         if (enterednumber) {
-            let enteredvalue = enterednumber!.value;
+            const enteredvalue = enterednumber!.value.replace(/\D/g, '');
+            enterednumber.value = enteredvalue;
+
+            if (enteredvalue.length == 10) {
+                setOTPbuttonstate(false);
+            } else {
+                setOTPbuttonstate(true);
+            }
             
             if (enteredvalue.length == 0) {
                 resetbutton!.style.display = "none";
@@ -35,11 +44,19 @@ const Phno = () => {
 
     function boarderremove() {
     boxboarder!.style.border = "1px solid #B2AEB9";
+
+    if(enterednumber!.value.length > 0 && enterednumber!.value.length < 10) {
+        setinvalidphnoerr("Please Enter a valid 10-digit phone number")
+    }else{
+        setinvalidphnoerr(null)
+    }
     };
 
     function clrbtn() {
     enterednumber!.focus();
     resetbutton!.style.display = "none";
+    setinvalidphnoerr(null);
+    setOTPbuttonstate(true);
     };
 
 
@@ -48,24 +65,26 @@ const Phno = () => {
 
             <p className='loginwelmsg'>Let's get you started</p>
             
+            {invalidphnoerr && <p className='errormsgph'>{invalidphnoerr}</p>}
+
             <div className="numberboxholder">
                 <form className="numberboxbar" id="numberboxbar">
                     <div className='telnoprefixholder'>
                         <p className="telnoprefix">+91</p>
                         <img className='prefixdivider' src={prefixdivider} alt="|" />
                     </div>
-                    <input className="numberbox" id="numberbox" type="tel" onFocus={boarderadd} onInput={valuecheck} onBlur={boarderremove} placeholder="Search for food and restaurants" />
+                    <input className="numberbox" id="numberbox" type="tel" maxLength={10} minLength={10} onFocus={boarderadd} onInput={valuecheck} onBlur={boarderremove} placeholder="Your Phone Number"/>
                     <button className="telrbtn" id="telrbtn" type="reset" onClick={clrbtn}>
                         <img className="resetlogo" src={reset} />
                     </button>
                 </form>
             </div>
 
-            <button className='getotpbutton'>Get OTP</button>
+            <button id='getotpbutton' className='getotpbutton' disabled={OTPbuttonstate} onClick={() => console.log("Success")}>Get OTP</button>
 
             <p className='TandC'>By proceeding, I accept the<br/>
-                <a href='https://tipplr.in/terms-conditions/' target="_blank"><b>Terms & Conditions</b></a> and
-                <a href='https://tipplr.in/privacy-policy/' target='_blank'><b> Privacy Policy</b></a>
+                <a href='https://tipplr.in/terms-conditions/' target="_blank"><b>Terms & Conditions</b></a> and&nbsp;
+                <a href='https://tipplr.in/privacy-policy/' target='_blank'><b>Privacy Policy</b></a>
             </p>
 
         </div>
