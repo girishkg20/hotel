246eaa6d3928c73baabfd2473a84aef39ca879dd
girishkg20 <girishkg20@gmail.com>
2023-12-18 20:08:50 +0530

    routing cache issue fixed

--- src/Home Page/HomePage1.tsx
+++ src/Home Page/HomePage1.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import KeepAlive, { useAliveController } from 'react-activation';
+import KeepAlive from 'react-activation';
 
 import Homepageapi from './Home_Page_API/HomePageApi.jsx';
 import Homepage from './Home_Page/HomePage';
@@ -7,12 +7,6 @@ import Homepage from './Home_Page/HomePage';
 
 const Homepage1 = () => {
 
-  const {dropScope} = useAliveController();
-
-  useEffect( () => {
-    dropScope("menu");
-  },[]);
-  
   
   return (
     <KeepAlive cacheKey='home'>
--- src/Menu Page/MenuPage1.tsx
+++ src/Menu Page/MenuPage1.tsx
@@ -1,5 +1,3 @@
-import KeepAlive from "react-activation";
-
 import Menupage from "./Menu_Page/MenuPage";
 import Menupageapi from "./Menu_Page_API/MenuPageApi";
 
@@ -9,11 +7,9 @@ import Menupageapi from "./Menu_Page_API/MenuPageApi";
 const Menupage1 = () => {
 
     return(<>
-        <KeepAlive cacheKey='menu' name="menu">
-            <Menupageapi>
-                <Menupage/>
-            </Menupageapi>
-        </KeepAlive>
+        <Menupageapi>
+            <Menupage/>
+        </Menupageapi>
     </>)
 }
 export default Menupage1;
